989ca4e81febcdbe19c09d2dfee3fd30a009a28e
M.Inzamam Malik <malikasinger1@gmail.com>
2021-11-03 21:58:02 +0500

    today code

--- nodejs/03.express_crud_local/server.mjs
+++ nodejs/03.express_crud_local/server.mjs
@@ -1,23 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
-const app = express()
+import cors from 'cors';
 
+const app = express()
 const port = process.env.PORT || 3000
 
 
-
 let users = [];
+app.use(cors())
 app.use(express.json())
 app.use(morgan('short'))
 
 app.use((req, res, next) => {
-
   console.log("a request came", req.body);
-  next()
-})
-
-app.use((req, res, next) => {
-  console.log("a request came", Date.now());
   next()
 })
 
